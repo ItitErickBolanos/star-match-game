4a1170ec34f161b5ed2a57d5181822fa3e876dc8
Erick Bolanos <eric.bolanos.consultant@nielseniq.com>
2022-08-29 10:43:27 -0500

    Added react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { StarMatch } from './components/StarMatch/StarMatch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StarMatch />
+    </div>
+  );
+}
+
+export default App;
